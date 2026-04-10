f0e1f678674d9686d248060cff8dbc246fa2c0cb
Fabianqc <fabian05demayo@gmail.com>
2026-04-10 13:54:46 -0400

    add react router

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,10 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
-import App from './login.jsx'
+import Login from './pages/login.jsx'
+import PriceList from './pages/PriceList.jsx'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Login />} />
+        <Route path='/pricelist' element={<PriceList />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
---
+++ Frontend/src/pages/PriceList.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function PriceList(){
+    return(
+        <div>
+            <h1>PriceList</h1>
+        </div>
+    )
+}
