07af20ca48484a2f0ac5294385c5aaaec2cd25ef
Fabianqc <fabian05demayo@gmail.com>
2026-04-12 01:14:47 -0400

    Configuring the database, making preparations to implement JWT, refresh token, and token rotate

--- DB/codigo db.sql
+++ DB/codigo db.sql
@@ -4,6 +4,14 @@ CREATE TABLE users (
     password_hash TEXT,
     created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
+CREATE TABLE refresh_tokens (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    user_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    token_hash TEXT NOT NULL,
+    expires_at TIMESTAMP WITH TIME ZONE NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    revoked BOOLEAN DEFAULT false
+);
 
 CREATE TABLE ui_translations (
     id SERIAL PRIMARY KEY,
@@ -19,4 +27,7 @@ CREATE TABLE products (
     price NUMERIC(10,2),
     is_active BOOLEAN DEFAULT true,
     update_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
-)
+);
+
+CREATE INDEX idx_refresh_tokens_user ON refresh_tokens(user_id);
+CREATE INDEX idx_refresh_tokens_hash ON refresh_tokens(token_hash);
